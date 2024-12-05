f07fb95f8d622b64efa07d70d47cf48d0066b3fb
NickKipshidze <kipshidze.nick@gmail.com>
2024-12-05 22:04:13 +0400

    Add header file for block.h

---
+++ pistonsim/include/block.h
@@ -0,0 +1,23 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+extern const int BLOCK_ID_AIR;
+extern const int BLOCK_ID_POWER;
+extern const int BLOCK_ID_WIRE;
+
+extern int worldWidth;
+extern int worldHeight;
+
+struct Block {
+    int blockId;
+    int data;
+    float state;
+    bool active;
+    bool ghost;
+};
+
+void setBlock(struct Block *world, int x, int y, struct Block block);
+struct Block *getBlock(struct Block *world, int x, int y);
+int getBlockId(struct Block *world, int x, int y);
+
+#endif
