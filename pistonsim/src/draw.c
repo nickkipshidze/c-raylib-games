49c2b8c916f1ab4eb3c84222534232c3cedead37
NickKipshidze <kipshidze.nick@gmail.com>
2024-12-06 15:17:58 +0400

    Add dynamically linking wires & rounded source block

--- pistonsim/src/draw.c
+++ pistonsim/src/draw.c
@@ -1,4 +1,5 @@
 #include <raylib.h>
+#include <stdio.h>
 
 #include "block.h"
 #include "world.h"
@@ -8,28 +9,38 @@ const int WINDOW_HEIGHT = 512;
 
 void drawBlock(struct Block block, int x, int y) {
     if (block.blockId == BLOCK_ID_POWER) {
-        DrawRectangle(x, y, tileSize, tileSize, (Color){245, 28, 28, 255});
+        DrawRectangleRounded(
+            (Rectangle){x, y, tileSize, tileSize},
+            0.2, 9, (Color){245, 28, 28, 255}
+        );
     } else if (block.blockId == BLOCK_ID_WIRE) {
+        Color color = (Color){52, 19, 19, 255};
         if (block.active == true)
-            DrawRectangle(x+15, y+15, tileSize-30, tileSize-30, (Color){52 + (12 * block.data), 26, 26, 255});
-        else
-            DrawRectangle(x+20, y+20, tileSize-40, tileSize-40, (Color){52, 19, 19, 255});
+            color = (Color){52 + (12 * block.data), 26, 26, 255};
+        
+        unsigned int state = block.state;
+        if (((state >> 3) & 1) == 1)
+            DrawRectangle(x+22, y, 19, 41, color);
+        if (((state >> 2) & 1) == 1)
+            DrawRectangle(x+22, y+22, 42, 19, color);
+        if (((state >> 1) & 1) == 1)
+            DrawRectangle(x+22, y+22, 19, 42, color);
+        if (((state >> 0) & 1) == 1)
+            DrawRectangle(x, y+22, 41, 19, color);
+        if (state == 0)
+            DrawRectangle(x+22, y+22, 19, 19, color);
     }
 }
 
 void drawWorld(struct Block *world) {
-    for (int y = 0; y < worldHeight; y++) {
-        for (int x = 0; x < worldWidth; x++) {
+    for (int y = 0; y < worldHeight; y++)
+        for (int x = 0; x < worldWidth; x++)
             drawBlock(world[y*worldWidth+x], x*64, y*64);
-        }
-    }
 }
 
 void drawBgGrid(int x1, int y1, int x2, int y2, Color color) {
-    for (int y = y1; y < y2; y += tileSize) {
-        for (int x = x1; x < x2; x += tileSize) {
+    for (int y = y1; y < y2; y += tileSize)
+        for (int x = x1; x < x2; x += tileSize)
             if ((x / tileSize + y / tileSize) % 2 == 0)
                 DrawRectangle(x, y, tileSize, tileSize, color);
-        }
-    }
 }
