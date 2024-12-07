dc87a9bd2ea6bb6c99800ee2be7b10daad52aa8b
NickKipshidze <kipshidze.nick@gmail.com>
2024-12-07 17:41:55 +0400

    Change getBlockId function & update variable names

--- pistonsim/src/block.c
+++ pistonsim/src/block.c
@@ -1,22 +1,20 @@
 #include <stdbool.h>
 #include "block.h"
+#include "world.h"
 
 const int BLOCK_ID_AIR = 0x00;
 const int BLOCK_ID_POWER = 0x01;
 const int BLOCK_ID_WIRE = 0x02;
 
-int worldWidth = 128;
-int worldHeight = 128;
-
 struct Block;
 
 void setBlock(struct Block *world, int x, int y, struct Block block) {
-    world[y*worldWidth+x] = block;
+    world[y*WRLDWidth+x] = block;
 }
 
 struct Block *getBlock(struct Block *world, int x, int y) {
-    if (x >= 0 && x < worldWidth && y >= 0 && y < worldHeight)
-        return &world[y*worldWidth+x];
+    if (x >= 0 && x < WRLDWidth && y >= 0 && y < WRLDHeight)
+        return &world[y*WRLDWidth+x];
     else {
         static struct Block air = {.blockId = BLOCK_ID_AIR};
         return &air;
@@ -24,8 +22,5 @@ struct Block *getBlock(struct Block *world, int x, int y) {
 }
 
 int getBlockId(struct Block *world, int x, int y) {
-    if (x >= 0 && x < worldWidth && y >= 0 && y < worldHeight)
-        return world[y*worldWidth+x].blockId;
-    else
-        return BLOCK_ID_AIR;
+    return getBlock(world, x, y) -> blockId;
 }
