7b46af44c81d9f03ad8ade1fa2debf275fa39d57
NickKipshidze <kipshidze.nick@gmail.com>
2024-12-07 17:37:40 +0400

    Define global simulation variables & updateWorld function

--- pistonsim/include/world.h
+++ pistonsim/include/world.h
@@ -4,11 +4,18 @@
 #include <raylib.h>
 #include "block.h"
 
-extern const int tileSize;
+extern int WRLDWidth;
+extern int WRLDHeight;
+extern const int WRLDTileSize;
+
+extern float WRLDLastTick;
+extern float WRLDTickRate;
+extern bool WRLDTickPaused;
 
 void initWorld(struct Block **world);
 void printWorld(struct Block *world);
-void updateWorld(struct Block *world);
+void tickWorld(struct Block *world);
+void updateWorld(struct Block *world, Camera2D camera);
 int getWorldHoverIdx(Camera2D camera);
 
 #endif
