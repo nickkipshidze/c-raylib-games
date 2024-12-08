72c1e2f8f1184246496f660800748124d01cf6fd
NickKipshidze <kipshidze.nick@gmail.com>
2024-12-08 17:58:41 +0400

    Change resolution to non-constant & add temporary hotbar variables

--- pistonsim/include/draw.h
+++ pistonsim/include/draw.h
@@ -4,14 +4,18 @@
 #include <raylib.h>
 #include "block.h"
 
-extern const int WINDOW_WIDTH;
-extern const int WINDOW_HEIGHT;
+extern int WINDOW_WIDTH;
+extern int WINDOW_HEIGHT;
+
+extern int HOTBAR_SELECTED;
+extern struct Block HOTBAR_BLOCKS[9];
 
 extern bool DEBUG_MODE;
 
 void drawBlock(struct Block block, int x, int y);
 void drawWorld(struct Block *world, Camera2D camera);
 void drawBgGrid(int x1, int y1, int x2, int y2, Color color);
+void drawHotbarUI();
 void drawDebugUI(Camera2D camera);
 
 #endif
